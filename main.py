3cfde49bee1e0b072556176b9420718b0a8eb9b0
Alex Teiche <xelapond@gmail.com>
2010-04-10 00:25:33 -0700

    Final cleanup commit, removed extraneous stuff.

--- main.py
+++ main.py
@@ -3,75 +3,13 @@ import random
 import sys
 import re
 
-
 import pyglet
 from pyglet.gl import *
-
 
 
 '''
 Unfortunately the design of NC Code lends itself to a state machine, 
 so that's how this program works:/
-'''
-
-'''
-Some G Codes:
-
-# Denotes a code that needs to be implemented
-
-#G00 Rapid positioning
-# G01 Linear interpolation
-# G02 CW circular interpolation
-# G03 CCW circular interpolation
-G04 Dwell
-G05.1 Q1. Ai Nano contour control
-G05 P10000 HPCC
-G07 Imaginary axis designation
-G09 Exact stop check
-G10/G11 Programmable Data input/Data write cancel
-# G12 CW Circle Cutting
-# G13 CCW Circle Cutting
-G17 X-Y plane selection
-G18 X-Z plane selection
-G19 Y-Z plane selection
-## G20 Programming in inches
-## G21 Programming in mm
-# G28 Return to home position
-G30 2nd reference point return
-G31 Skip function (used for probes and tool length measurement systems)
-G33 Constant pitch threading
-G34 Variable pitch threading
-G40 Tool radius compensation off
-G41 Tool radius compensation left
-G42 Tool radius compensation right
-G43 Tool height offset compensation negative
-G44 Tool height offset compensation positive
-G45 Axis offset single increase
-G46 Axis offset single decrease
-G47 Axis offset double increase
-G48 Axis offset double decrease
-G49 Tool offset compensation cancel
-G50 Define the maximum spindle speed
-G53 Machine coordinate system
-G54 to G59 Work coordinate systems
-G54.1 P1 to P48 Extended work coordinate systems
-G73 High speed drilling canned cycle
-G74 Left hand tapping canned cycle
-G76 Fine boring canned cycle
-G80 Cancel canned cycle
-G81 Simple drilling cycle
-G82 Drilling cycle with dwell
-G83 Peck drilling cycle
-G84 Tapping cycle
-G84.2 Direct right hand tapping canned cycle
-### G90 Absolute programming (type B and C systems)
-### G91 Incremental programming (type B and C systems)
-# G92 Programming of absolute zero point
-G94/G95 Inch per minute/Inch per revolution feed (type A system) Note: Some CNCs use the SI unit system
-G96 Constant surface speed
-G97 Constant Spindle speed
-G98/G99 Return to Initial Z plane/R plane in canned cycle
-
 '''
 
 #Global states:(
@@ -262,7 +200,8 @@ def on_mouse_scroll(x, y, dx, dy):
     global gzl #This is only a global variable so we can multiply panning, to make it work on a 1:1 ratio at all zoom levels
     zl = 1 + (dy/10)
     gzl *= zl
-    print 'Zoom Level: ' + str(gzl)
+    if '-Z' not in sys.argv:
+        print 'Zoom Level: ' + str(gzl)
     #Translate to the mouse so we don't zoom about the origin
     glTranslatef(x, y, 0)
     glScalef(zl, zl, zl)
@@ -276,8 +215,6 @@ def on_draw(t, win, dlist):
     
     win.flip()
 
-width, height = 800, 800
-
 if __name__ == '__main__':
     #Get a G-Code file
     file = open(sys.argv[1])
@@ -290,66 +227,9 @@ if __name__ == '__main__':
     print 'Units: ' + {True:'Imperial', False:'Metric'}[inches]
 
     win = pyglet.window.Window(800, 800)
-    ''' 
-    glViewport(0, 0, width, height)
-    glMatrixMode(gl.GL_PROJECTION)
-    glLoadIdentity()
-    glFrustum(-width/2, width/2, -height/2, height/2, .1, 1000)
-    glScalef(5000,5000,1)
-    #glTranslatef(-width/2,-height/2,-500)
-    glTranslatef(0, 0, -500)
-    glMatrixMode(gl.GL_MODELVIEW)
-    '''
+
     win.on_mouse_drag = on_mouse_drag
     win.on_mouse_scroll = on_mouse_scroll
 
     pyglet.clock.schedule(on_draw, win, dlist)
     pyglet.app.run()
-
-#This was the first on_draw before proper parsing and display lists
-'''
-def on_draw(*argarg):
-    global win, nc
-    win.clear()
-    #glClear(GL_COLOR_BUFFER_BIT)
-
-    #dRect((0, 0), (500, 500), (0, 1, 0, 1))
-
-    glColor3f(1, 0, 0)
-    glBegin(GL_LINE_STRIP)
-
-    for line in nc:
-        x = y = z = None
-        exp = line.split(' ')
-        if exp[0] == 'G01' or exp[0] == 'G00':
-            try:
-                exp.remove('')
-            except:
-                pass
-            args = exp[1:]
-            
-            for arg in args:
-                if arg[0].lower() == 'x':
-                    x = float(arg[1:])
-                elif arg[0].lower() == 'y':
-                    y = float(arg[1:])
-                elif arg[0].lower() == 'z':
-                    z = float(arg[1:])
-                else:
-                    if '-w' in sys.argv:
-                        print 'FAIL ' + str(exp)
-                        
-            if z:
-                try:
-                    glColor3f(*cdict[z])
-                except:
-                    glColor3f(*cdict['EXCEPTION'])
-            if x and y:
-                glVertex2f(x*zl, y*zl)
-
-    
-
-    glEnd()
-    
-    win.flip()
-'''
